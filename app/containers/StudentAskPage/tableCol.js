6cc87f652684c07fa59800b952f0b9bd6c66f61a
tunggao <ntung13698@gmail.com>
2020-03-05 09:12:27 +0700

    upload but doent work anything

--- app/containers/StudentAskPage/tableCol.js
+++ app/containers/StudentAskPage/tableCol.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
-const colunms = [
-    {
-        // dataIndex: "key",
-        width: 50,
-    },
-    {
-        title: "TUTOR",
-        dataIndex: "teacher",
-        width: 175,
-    },
-    {
-        title: "E-MAIL",
-        dataIndex: "question",
-        width: 600,
-    },
-    {
-        title:"Time",
-        dataIndex:"date",
-    },
+const colunms = [{
+    // dataIndex: "key",
+    width: 50,
+  },
+  {
+    title: "TUTOR",
+    dataIndex: "teacher",
+    render: text => <span style={{ color: '#1593E6', fontSize: 15, fontWeight: 600 }}>{text}</span>,
+    width: 175,
+  },
+  {
+    title: "E-MAIL",
+    render: (text, record) =>{
+        console.log(record)
+        return(<div>
+        <span className="answered-icon" size="medium" style={{fontSize: 15}}></span>
+        <p>{text}</p>
+    </div>)},
+    dataIndex: "question",
+    width: 600,
+  },
+  {
+    title: "Time",
+    dataIndex: "date",
+  },
 ];
 
 export default colunms;

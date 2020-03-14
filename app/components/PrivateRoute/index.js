20732f5527bcc2256324f9838a9997b7bb753b4d
cuongnq072610 <cuongnqse05240@fpt.edu.vn>
2020-03-14 13:44:24 +0700

    demo privateRoute

--- app/components/PrivateRoute/index.js
+++ app/components/PrivateRoute/index.js
@@ -7,10 +7,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={props => (
     localStorage.getItem('user')
       ? <Component {...props} />
       : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
   )} />
 )
+
+export default PrivateRoute;

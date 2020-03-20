f52c7401f8dd128bed42868a12bb21d8864ac831
cuongnq072610 <cuongnqse05240@fpt.edu.vn>
2020-03-20 09:41:40 +0700

    fix login action

--- app/containers/LoginPage/saga.js
+++ app/containers/LoginPage/saga.js
@@ -2,12 +2,9 @@ import { take, call, put, select, takeLatest } from 'redux-saga/effects';
 import { LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS } from './constants';
 import { loginService } from './api';
 import { API_ENDPOINT, LOGIN_API } from '../../constants/apis';
-import history from '../../utils/history';
-import parseJwt from '../../utils/parseJWT';
 
 function* handleLogin(action) {
   const { email, password } = action;
-  console.log(email + "--" + password)
   try {
     let response = yield call(loginService, `${API_ENDPOINT}${LOGIN_API}`, { email, password });
     if (response.data.err) {

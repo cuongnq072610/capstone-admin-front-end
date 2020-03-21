34398a5fca282016ec9d42f322ddaacd0bd1504b
cuongnq072610 <cuongnqse05240@fpt.edu.vn>
2020-03-21 15:46:55 +0700

    fix

--- app/containers/StudentAskPage/tableCol.js
+++ app/containers/StudentAskPage/tableCol.js
@@ -1,39 +1,41 @@
 import React from 'react';
 
-const colunms = [{
-  // dataIndex: "key",
-  width: 50,
-},
-{
-  title: "TUTOR",
-  dataIndex: "teacher.teacherName",
-  render: text => <span style={{ color: '#1593E6', fontSize: 17, fontWeight: 600 }}>{text}</span>,
-  width: 175,
-  key: 'teacher.teacherName'
-},
-{
-  title: "QUESTION",
-  render: (text, record) => {
-    return (
-      <div className="ask-content">
-        {
-          record.isReaded ?
-            <span className="ask-icon ask-icon-read" ></span> :
-            <span className="ask-icon ask-icon-unread" ></span>
-        }
-        <p className={`ask-content-ques ${record.isReaded && "read"}`}>{text}</p>
-      </div>
-    )
+const colunms = [
+  {
+    dataIndex: "teacher.avatar",
+    width: 50,
+    render: text => <img src={text} style={{ width: '30px', height: '30px' }} />
   },
-  dataIndex: "askContent",
-  width: 600,
-  key: 'askContent'
-},
-{
-  title: "Time",
-  dataIndex: "dateModified",
-  key : 'dateModified'
-},
+  {
+    title: "TUTOR",
+    dataIndex: "teacher.name",
+    render: text => <span style={{ color: '#1593E6', fontSize: 17, fontWeight: 600 }}>{text}</span>,
+    width: 175,
+    key: 'teacher.name'
+  },
+  {
+    title: "QUESTION",
+    render: (text, record) => {
+      return (
+        <div className="ask-content">
+          {
+            record.isReaded ?
+              <span className="ask-icon ask-icon-read" ></span> :
+              <span className="ask-icon ask-icon-unread" ></span>
+          }
+          <p className={`ask-content-ques ${record.isReaded && "read"}`}>{text}</p>
+        </div>
+      )
+    },
+    dataIndex: "askContent",
+    width: 600,
+    key: 'askContent'
+  },
+  {
+    title: "Time",
+    dataIndex: "dateModified",
+    key: 'dateModified'
+  },
 ];
 
 export default colunms;

823da376dc72b01a72fb35212db75f0f3ad3cada
cuongnq072610 <cuongnqse05240@fpt.edu.vn>
2020-04-17 20:52:50 +0700

    create not found page + unit test homepage

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -8,14 +8,25 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
+import H2 from 'components/H2';
+
 import messages from './messages';
+import './index.scss';
+import notFoundLogo from './assets/noteIt-404.png';
+import noteItCyan from './assets/noteIt-cyan.png';
 
 export default function NotFound() {
   return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
+    <div className='not-found-page'>
+      <div className='not-found-wrapper'>
+        <img src={notFoundLogo} className='not-found-logo' />
+        <H1 className='warning' style={{ fontWeight: '900' }}><FormattedMessage {...messages.head} /></H1>
+        <H2 className='warning'><FormattedMessage {...messages.body} /></H2>
+        <div className='box-warning'>
+          <img src={noteItCyan} className='note-logo' />
+          <p className='box-p'><FormattedMessage {...messages.footer} /></p>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/containers/NotFoundPage/tests/index.test.js
+++ app/containers/NotFoundPage/tests/index.test.js
@@ -7,19 +7,43 @@ import { shallow } from 'enzyme';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
+import H2 from 'components/H2';
+
 import NotFound from '../index';
+import notFoundLogo from './assets/noteIt-404.png';
+import noteItCyan from './assets/noteIt-cyan.png';
 
 describe('<NotFound />', () => {
   it('should render the Page Not Found text', () => {
     const renderedComponent = shallow(<NotFound />);
     expect(
       renderedComponent.contains(
-        <H1>
-          <FormattedMessage
-            id="boilerplate.containers.NotFoundPage.header"
-            defaultMessage="Page not found."
-          />
-        </H1>,
+        <div className='not-found-page'>
+          <div className='not-found-wrapper'>
+            <img src={notFoundLogo} className='not-found-logo' />
+            <H1 className='warning' style={{ fontWeight: '900' }}>
+              <FormattedMessage
+                id="boilerplate.containers.NotFoundPage.head"
+                defaultMessage="404 - Rrrrrr this is Dino`s note!"
+              />
+            </H1>
+            <H2 className='warning'>
+              <FormattedMessage
+                id="boilerplate.containers.NotFoundPage.body"
+                defaultMessage="We could not find what you want to look for, sorry for not noting it."
+              />
+            </H2>
+            <div className='box-warning'>
+              <img src={noteItCyan} className='note-logo' />
+              <p className='box-p'>
+                <FormattedMessage
+                  id="boilerplate.containers.NotFoundPage.footer"
+                  defaultMessage="Head back to us now!"
+                />
+              </p>
+            </div>
+          </div>
+        </div>,
       ),
     ).toEqual(true);
   });
